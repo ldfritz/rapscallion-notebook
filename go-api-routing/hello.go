d526b66d297c95901ab4801d6f2286a23f3639e3
Luke Fritz <ldfritz@gmail.com>
2017-06-25 21:07:42 -0400

    Add some comments to the api-routing main file.
      Add a few comments to start clarify what exactly is going on in the
    api-routing example.

--- go-api-routing/hello.go
+++ go-api-routing/hello.go
@@ -1,5 +1,6 @@
 package main
 
+// Import each of the APIs (dev and v1) and some helpers.
 import (
 	"github.com/ldfritz/rapscallion-notebook/go-api-routing/dev"
 	"github.com/ldfritz/rapscallion-notebook/go-api-routing/helpers"
@@ -19,14 +20,24 @@ type Root struct{}
 // in the switch statement.  Invalid routes throw an error.
 func (a Root) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 	log.Printf("Root.ServeHTTP(): %s %s", req.Method, req.URL.String())
+	// For more see: ./helpers/shiftpath.go
+	// This function takes the path portion of the URL string.  It
+	// splits it into the first folder and the rest.  For example,
+	// `/dev/user/1/files` becomes `dev` and `/user/1/files`.
 	var head string
 	head, req.URL.Path = helpers.ShiftPath(req.URL.Path)
 	switch head {
+	// Here we pass the trimmed request on to the API package.  We
+	// don't care what is even defined in the API at this point.
+	// These APIs would vary wildly, but we don't have to worry
+	// about that except in the API's package.
 	case "dev":
 		dev.Root(res, req)
 	case "v1":
 		v1.Root(res, req)
 	default:
+		// Pause invalid requests as early in the tree as
+		// possible.
 		http.Error(res, "Not Found", http.StatusNotFound)
 	}
 }
@@ -37,6 +48,7 @@ func (a Root) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 // above.
 func main() {
 	port := ":8080"
+	// Without this line, the start of the server is silent.
 	log.Printf("Starting server on %s (^C to stop)", port)
 	log.Fatal(http.ListenAndServe(port, &Root{}))
 }
